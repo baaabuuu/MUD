c39024d5ccbb250f4bf43a54e2f055139057f730
Christian <Christian@192.168.1.54>
2017-01-04 13:29:36 +0100

    MORE!

--- Client/Client/Cl_Main.java
+++ Client/Client/Cl_Main.java
@@ -1,13 +1,30 @@
 package Client;
 
+import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Cl_Main extends JFrame implements KeyListener{
-
-	
+	// Declare components for the program's window
+		public JButton btn1;
+		public JLabel lbl1, lbl2, lbl3;
+		public JTextField txtfld1, txtfld2, txtfld3;
+	Cl_Main(){
+		// Set a BorderLayout on the main window
+		getContentPane().setLayout(new BorderLayout());
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 	
