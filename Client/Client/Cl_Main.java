225cf85cde08ff17b233ef8c997120db2eebbce8
Christian <Christian@Herreborg-PC.clients.wireless.dtu.dk>
2017-01-06 12:57:16 +0100

    Typing Area

--- Client/Client/Cl_Main.java
+++ Client/Client/Cl_Main.java
@@ -1,6 +1,8 @@
 package Client;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -23,12 +25,12 @@ import java.awt.Label;
 import java.awt.Color;
 import java.awt.Toolkit;
 
-public class Cl_Main extends JFrame implements KeyListener{
+public class Cl_Main extends JFrame implements ActionListener, KeyListener{
 
 	private JPanel contentPane;
 	public JButton actionSend, chatSend;
 	public JLabel lblMainTitle, lblPlayerHealth, lblChat, lblTemp1, lblTemp2, lblRemainingWordsChat, lblRemainingWordsAction;
-	public JTextArea eventArea, actionArea, chatRecieveArea, chatTypingArea;
+	public JTextArea eventArea, actionArea, chatRecArea, chatTypArea;
 	private DefaultStyledDocument docAction, docChat;
 	private Label label_1;
 
@@ -76,7 +78,7 @@ public class Cl_Main extends JFrame implements KeyListener{
 			public void removeUpdate(DocumentEvent e) {updateCount(2);}
         });
 		
-		eventArea = new JTextArea();
+		eventArea = new JTextArea("Welcome Adventurer!");
 		eventArea.setToolTipText("Read me!");
 		eventArea.setLineWrap(true);
 		eventArea.setEditable(false);
@@ -91,28 +93,30 @@ public class Cl_Main extends JFrame implements KeyListener{
 		actionArea.setLineWrap(true);
 		actionArea.setWrapStyleWord(true);
 		actionArea.setToolTipText("Type me!");
+		actionArea.addKeyListener(this);
 		actionArea.setBounds(10, 500, 500, 75);
 		actionArea.setDocument(docAction);
 		contentPane.add(actionArea);
 		
 		
-		chatRecieveArea = new JTextArea();
-		chatRecieveArea.setToolTipText("Read me!");
-		chatRecieveArea.setLineWrap(true);
-		chatRecieveArea.setEditable(false);
+		chatRecArea = new JTextArea("Welcome to the chat!");
+		chatRecArea.setToolTipText("Read me!");
+		chatRecArea.setLineWrap(true);
+		chatRecArea.setEditable(false);
 		
-		JScrollPane chatRecieveAreaScroll = new JScrollPane(chatRecieveArea);
+		JScrollPane chatRecieveAreaScroll = new JScrollPane(chatRecArea);
 		chatRecieveAreaScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		chatRecieveAreaScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		chatRecieveAreaScroll.setBounds(800, 51, 465, 440);
 		contentPane.add(chatRecieveAreaScroll);
 		
-		chatTypingArea = new JTextArea();
-		chatTypingArea.setLineWrap(true);
-		chatTypingArea.setToolTipText("Type me!");
-		chatTypingArea.setBounds(800, 500, 365, 75);
-		chatTypingArea.setDocument(docChat);
-		contentPane.add(chatTypingArea);
+		chatTypArea = new JTextArea();
+		chatTypArea.setLineWrap(true);
+		chatTypArea.setToolTipText("Type me!");
+		chatTypArea.addKeyListener(this);
+		chatTypArea.setBounds(800, 500, 365, 75);
+		chatTypArea.setDocument(docChat);
+		contentPane.add(chatTypArea);
 		
 		
 		actionSend = new JButton("Send");
@@ -141,7 +145,7 @@ public class Cl_Main extends JFrame implements KeyListener{
 		lblNewLabel_3.setBounds(615, 81, 175, 14);
 		contentPane.add(lblNewLabel_3);
 		
-		Label label = new Label("Game Area");
+		Label label = new Label("Event Area");
 		label.setBounds(10, 22, 200, 22);
 		contentPane.add(label);
 		
@@ -160,16 +164,48 @@ public class Cl_Main extends JFrame implements KeyListener{
 		}
 		
     }
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	private String eventAreaInfo;
+	public void updEventArea(String text){
+		eventAreaInfo = eventArea.getText();
+		eventAreaInfo += "\n" + text;
+		eventArea.setText(eventAreaInfo);
 	}
+	private String chatRecAreaInfo;
+	public void updChatArea(String text){
+		chatRecAreaInfo = chatRecArea.getText();
+		chatRecAreaInfo += "\n" + text;
+		chatRecArea.setText(chatRecAreaInfo);
+	}
+	public void keyTyped(KeyEvent e) {}
+	private String actionAreaTemp;
+	private String chatTypAreaTemp;
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		//On ENTER pressed in actionArea, create temporary string without newline
+		//and update actionArea without newline.
+		if(e.getSource() == actionArea && e.getKeyCode() == KeyEvent.VK_ENTER){
+			actionAreaTemp = actionArea.getText().replace("\n","");
+			actionArea.setText(actionAreaTemp);
+		}else if(e.getSource() == chatTypArea && e.getKeyCode() == KeyEvent.VK_ENTER){
+			chatTypAreaTemp = chatTypArea.getText().replace("\n","");
+			chatTypArea.setText(chatTypAreaTemp);
+		}
 	}
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		//On ENTER release from actionArea, update eventArea with temporary
+		//text and reset actionArea.
+		if(e.getSource() == actionArea && e.getKeyCode() == KeyEvent.VK_ENTER){
+			updEventArea("Player: " + actionAreaTemp);
+			actionArea.setText("");
+		}else if(e.getSource() == chatTypArea && e.getKeyCode() == KeyEvent.VK_ENTER){
+			updChatArea("" + chatTypAreaTemp);
+			chatTypArea.setText("");
+		}
+	}
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == actionSend){
+			updEventArea(actionArea.getText());
+		}else if(e.getSource() == chatTypArea || e.getSource() == chatSend){
+			updChatArea(chatTypArea.getText());
+		}
 	}
 }
