202189bb81df8e20be7580b9246965169a168b1d
baaabuuu <aradar@live.dk>
2017-01-09 16:27:54 +0100

    You can now walk between events - TODO between rooms.

--- Worlds/dungeons/Room.java
+++ Worlds/dungeons/Room.java
@@ -16,6 +16,7 @@ public class Room
 	
 	public enum Event {
 		FIGHT,
+		EXIT,
 		TEXT;
 		
 	}
@@ -25,18 +26,29 @@ public class Room
 	 * <p>0 = first time entering a room.
 	 * <p>1	= when entering the room afterwards.
 	 * <p>Other IDs have different functions and can be refered to in different ways.
-	 */
+	 */	
 	@SuppressWarnings("rawtypes")
 	public ArrayList<ArrayList> eventStack			=	new ArrayList<ArrayList>();
 	
+	public ArrayList<String>	exitOption			=	new ArrayList<String>();
+	
+	public ArrayList<Integer>	exitID				=	new ArrayList<Integer>();
 	
 	@SuppressWarnings("rawtypes")
 	public ArrayList<ArrayList> eventTextStack		=	new ArrayList<ArrayList>();
 	
 	@SuppressWarnings("rawtypes")
 	public ArrayList<ArrayList> eventOptionStack	=	new ArrayList<ArrayList>();
+	
 	@SuppressWarnings("rawtypes")
 	public ArrayList<ArrayList> eventOptionIDStack	=	new ArrayList<ArrayList>();
+	
+	
+	@SuppressWarnings("rawtypes")
+	public ArrayList<ArrayList> eventExitStack	=	new ArrayList<ArrayList>();
+	
+	@SuppressWarnings("rawtypes")
+	public ArrayList<ArrayList> eventExitID		=	new ArrayList<ArrayList>();
 	
 	public void newTextEvent(String text, ArrayList<Event> events, ArrayList<String> textList)
 	{		
@@ -48,11 +60,18 @@ public class Room
 	{
 		textList.add(text);
 		id2.add(id);
-		
+	}
+	
+	public void newExit(String text, int id, ArrayList<Event> events, ArrayList<String> textList, ArrayList<Integer> id2)
+	{
+		events.add(Event.EXIT);
+		textList.add(text);
+		id2.add(id);
 	}
 	
 	/**
 	 * Adds a new event to our event stack.
+	 * <p>When adding new event types remember to add them here as well
 	 */
 	public void newEvent()
 	{
@@ -60,37 +79,49 @@ public class Room
 		eventTextStack.add(new ArrayList<String>());
 		eventOptionStack.add(new ArrayList<String>());
 		eventOptionIDStack.add(new ArrayList<Integer>());
+		eventExitStack.add(new ArrayList<String>());
+		eventExitID.add(new ArrayList<Integer>());
 		
 	}
 
-	public void runEvent(int id)
+	@SuppressWarnings({"unchecked" })
+	public String runEvent(int id)
 	{
 		ArrayList<Event>	events		=	eventStack.get(id);
 		ArrayList<String>	text		=	eventTextStack.get(id);
 		ArrayList<String>	options		= 	eventOptionStack.get(id);
 		ArrayList<Integer>	nextEvent	=	eventOptionIDStack.get(id);
-		options.add("Exit");
-		nextEvent.add(-1);
+		ArrayList<String>	exits		=	eventExitStack.get(id);
+		ArrayList<Integer>	exitIDs		=	eventExitID.get(id);
 		
 		int 				textCounter	=	0;
+		int 				exitCounter	=	0;
+		if (!options.contains("Exit"))
+		{
+			options.add("Exit");
+			nextEvent.add(-1);
+		}
 		
 		for (Event event : events)
 		{
-			switch (event){					
+			switch (event){
+				case EXIT:
+					exitOption.add(exits.get(exitCounter));
+					exitID.add(exitIDs.get(exitCounter));
+					exitCounter++;
+					break;
 				case FIGHT:
 					break;
 				case TEXT: // prints the first line in eventDescription and then deletes it.
 					try {
 						Thread.sleep(750);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					System.out.println(text.get(textCounter));
 					try {
 						Thread.sleep(750);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					textCounter++;
@@ -101,42 +132,94 @@ public class Room
 		//Things can occur here
 		//run game logic
 		Scanner input = new Scanner(System.in);
-		System.out.println("Select which option you'd like to take:");	
-		for (String option : options)
-		{
-			System.out.println(" --- <" + option +">" );
-		}
 		String newInput;
+		String answer;
 		boolean correctValue;
+		boolean correctValue2;
+		int		newEvent	=	-1;
 		while(true)
 		{
-			
+			System.out.println("Select which option you'd like to take:");	
+			for (String option : options)
+			{
+				System.out.println(" --- <" + option +">" );
+			}
 			newInput	=	input.nextLine().toLowerCase();
 			if (newInput.startsWith("<"))
 				newInput	=	newInput.substring(1, newInput.length());
 			if (newInput.endsWith(">"))
 				newInput	=	newInput.substring(0, newInput.length()-1);
 				
-			correctValue	=	true;
+			correctValue	=	false;
 			for (String option : options)
 			{
-				System.out.println(option.toLowerCase() + " = " + newInput.toLowerCase());
 				if (option.toLowerCase().equals(newInput.toLowerCase()))
 				{	correctValue	=	true;
 					break;
 				}
 			}
+			
+			for (int i = 0; i<exitOption.size();i++)
+			{
+				if (options.get(i).toLowerCase().equals(newInput))
+				{
+					correctValue	=	true;
+					newEvent		=	nextEvent.get(i);
+				}
+			}
+			
+			
 			if (correctValue)
 			{
-				break;
+				if (newInput.equals("exit"))
+				{
+					System.out.println("Where would you like to exit through:");	
+					for (String option : exitOption)
+					{
+						System.out.println(" --- <" + option +">" );
+					}
+					while (true)
+					{
+						answer = input.nextLine().toLowerCase();
+						if (answer.startsWith("<"))
+							answer	=	answer.substring(1, answer.length());
+						if (newInput.endsWith(">"))
+							answer	=	answer.substring(0, answer.length()-1);
+						if (!answer.equals("none"))
+						{
+							for (int i = 0; i<exitOption.size();i++)
+							{
+								if (answer.equals(exitOption.get(i).toLowerCase()))
+								{
+									return "exit@" + exitID.get(i);
+								}
+							}
+						}
+						else //if answer == "none" - Yes this is not valid java syntax
+						{
+							//in case answer is equal to false exit - tell the 
+							//system to rerun select option screen
+							correctValue	=	false;
+							break;
+						}	
+					}	
+				}
+				if (correctValue)
+					break;
+				
 			} else
 			{
 				System.out.println("WRONG INPUT, TRY AGAIN");
 			}
-			
 		}
+		//returns newEvent@ID where ID is the new event to run.
+		return "newEvent@"+newEvent;
 	}	
-	
+	public Room()
+	{
+		exitOption.add("None");
+		exitID.add(-1);
+	}
 	
 	
 	
--- Worlds/dungeons/World.java
+++ Worlds/dungeons/World.java
@@ -16,42 +16,60 @@ public class World {
 	
 	/**
 	 * Creates Test Town
+	 * <P> Please note that events run till they require an input - branching events require
+	 * different events for each path for that reason.
 	 */
+	//used to get rid of annoying yellow flags
+	@SuppressWarnings("unchecked")
 	public static void createTestTown(Dungeon dung)
 	{
 		dung.newRoom();
-		Room room	=	dung.newRoom;
+		Room 				room		=	dung.newRoom;
+		Room				playerRoom	=	room;
 		room.newEvent();
-		ArrayList<Event>	event				=	room.eventStack.get(room.eventStack.size()-1);
-		ArrayList<String>	textEvent			=	room.eventTextStack.get(room.eventTextStack.size()-1);
+		ArrayList<Event>	event			=	room.eventStack.get(room.eventStack.size()-1);
+		ArrayList<String>	textEvent		=	room.eventTextStack.get(room.eventTextStack.size()-1);
 		ArrayList<String>	optionEvent		=	room.eventOptionStack.get(room.eventOptionStack.size()-1);
 		ArrayList<Integer>	optionIDevent	=	room.eventOptionIDStack.get(room.eventOptionIDStack.size()-1);
-		room.newTextEvent("Testing 1.",event,textEvent);
-		room.newTextEvent("Testing 2.",event,textEvent);
-		room.newTextEvent("Testing 3.",event,textEvent);
-		room.newTextEvent("Testing 4.",event,textEvent);
-		room.newTextEvent("Testing 5.",event,textEvent);
-		room.newTextEvent("Testing 6.",event,textEvent);
-		room.newOptionEvent("North", -1, optionEvent, optionIDevent);
-		room.newOptionEvent("South", -1, optionEvent, optionIDevent);
-		room.newOptionEvent("East", -1, optionEvent, optionIDevent);
-		room.newOptionEvent("Plane", -1, optionEvent, optionIDevent);
+		ArrayList<String>	exitEvent		=	room.eventExitStack.get(room.eventExitStack.size()-1);
+		ArrayList<Integer>	exitEventID		=	room.eventExitID.get(room.eventExitID.size()-1);
+		
+		//this is the order in which events occur.
+		room.newTextEvent("Running event 0.",event,textEvent);
+		room.newOptionEvent("Run event 1", 1, optionEvent, optionIDevent);
+
+		
+		//adds the exit if event 0 launches
+		room.newExit("Market",1,event,exitEvent,exitEventID);
 		
 		
 		room.newEvent();
-		event	=	room.eventStack.get(room.eventStack.size()-1);
-		textEvent	=	room.eventTextStack.get(room.eventTextStack.size()-1);
+		event			=	room.eventStack.get(room.eventStack.size()-1);
+		textEvent		=	room.eventTextStack.get(room.eventTextStack.size()-1);
+		optionEvent		=	room.eventOptionStack.get(room.eventOptionStack.size()-1);
+		optionIDevent	=	room.eventOptionIDStack.get(room.eventOptionIDStack.size()-1);
+		exitEvent		=	room.eventExitStack.get(room.eventExitStack.size()-1);
+		exitEventID		=	room.eventExitID.get(room.eventExitID.size()-1);
 		
-		room.newTextEvent("fuck 1.",event,textEvent);
-		room.newTextEvent("this 2.",event,textEvent);
-		room.newTextEvent("shit 3.",event,textEvent);
-		room.newTextEvent("up 4.",event,textEvent);
-		room.newTextEvent("your 5.",event,textEvent);
-		room.newTextEvent("ass 6.",event,textEvent);
+		room.newTextEvent("Running event 1.",event,textEvent);
+		room.newOptionEvent("Run event 0", 0, optionEvent, optionIDevent);
 		//call this when entering a new room
-		room.runEvent(1);
-		System.out.println("RUNNING EVENT STACK 2");
-		room.runEvent(0);
+		int dungID	=	0;
+		String[]	dungeon;
+		while(true)
+		{
+			dungeon	=	playerRoom.runEvent(dungID).split("@");
+			if (dungeon[0].equals("exit"))
+			{
+				//switch room
+				break;
+			}
+			else if (dungeon[0].equals("newEvent"))
+			{
+				dungID	=	Integer.parseInt(dungeon[1]);
+			}
+		}
+		
 	}
 	
 }
